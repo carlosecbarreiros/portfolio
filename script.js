070e7275c97db258a66ba0488db5b902377b04a9
Carlos Barreiros <137417158+cadu1407@users.noreply.github.com>
2024-01-09 19:32:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+  // Função para rolagem suave ao clicar em um link com hash
+  function scrollToElement(elementId) {
+    const element = document.getElementById(elementId);
+    const elementHeight = element.offsetHeight;
+    const windowHeight = window.innerHeight;
+
+    const container = document.querySelector('.container');
+    const containerHeight = container.offsetHeight;
+
+    const offset = element.offsetTop - container.offsetTop;
+    const targetScrollPosition = offset - (windowHeight - elementHeight) / 2 + containerHeight / 3 + (elementHeight / 30) + 170; 
+
+    window.scrollTo({
+        top: targetScrollPosition,
+        behavior: 'smooth'
+    });
+}
+
+// Adicione o manipulador de evento de clique para os links com hash
+document.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A' && event.target.getAttribute('href').startsWith('#')) {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href').slice(1);
+        scrollToElement(targetId);
+    }
+});
